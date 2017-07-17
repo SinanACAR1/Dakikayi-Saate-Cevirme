c5a5b8083673b4d906a961d3bbee664de8fc5556
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 21:15:03 +0300

    Add files via upload

---
+++ Dakikayı Saate çevirme/Dakikayı Saate çevirme/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Dakikayı_Saate_Saniyeyi_Dakikaya_çeviren_program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int dakika, saat;
+            int sayı;
+            sayı = Convert.ToInt32(textBox1.Text);
+
+
+            saat = sayı / 60;
+            label1.Text = saat.ToString();
+
+            dakika = sayı % 60;
+            label2.Text = dakika.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {        
+                   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
